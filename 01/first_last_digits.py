8a20f6f3214b7af031f1b26bffb7a4bcc1b4bbf4
Ian Roberts <i.roberts@dcs.shef.ac.uk>
2023-12-01 17:00:32 +0000

    Part 2 code can be vastly simplified if we add the digits themselves to the mapping dict

--- 01/first_last_digits.py
+++ 01/first_last_digits.py
@@ -1,6 +1,10 @@
 import re
 import sys
 
+
+# Part 1: find the first and last *digits* in each line, combine them as a
+# two digit number and sum across all lines
+
 def digits_only():
     calibration_sum = 0
     with open(sys.argv[1], "r") as f:
@@ -11,6 +15,11 @@ def digits_only():
 
     print(f"Sum (digits only) = {calibration_sum}")
 
+
+# Part 2: as well as digits, look for digit-as-word.  So now the first "digit"
+# is either a true digit or a word "one" to "nine", and the last digit is the
+# first instance in the reversed string of either a true digit or the reverse
+# spelling of "eno" to "enin"
 
 forward_digits = {
     'one': 1,
@@ -23,27 +32,23 @@ forward_digits = {
     'eight': 8,
     'nine': 9,
 }
-forward_re = re.compile(r"(" + "|".join(forward_digits.keys()) + r")|\d")
+# Add mappings for the actual digits 0-9
+forward_digits.update((str(i), i) for i in range(10))
 
+forward_re = re.compile("|".join(forward_digits.keys()))
+
+# Create a parallel mapping of the reverse of each digit string
+# for lookup in the reversed input string
 reverse_digits = {word[::-1]: value for word, value in forward_digits.items()}
-reverse_re = re.compile(r"(" + "|".join(reverse_digits.keys()) + r")|\d")
+reverse_re = re.compile("|".join(reverse_digits.keys()))
 
 
 def digits_and_words():
     calibration_sum = 0
     with open(sys.argv[1], "r") as f:
         for line in f:
-            match = forward_re.search(line)
-            if match.group(1):
-                first_digit = forward_digits[match.group(1)]
-            else:
-                first_digit = int(match.group())
-
-            match = reverse_re.search(line[::-1])
-            if match.group(1):
-                last_digit = reverse_digits[match.group(1)]
-            else:
-                last_digit = int(match.group())
+            first_digit = forward_digits[forward_re.search(line).group()]
+            last_digit = reverse_digits[reverse_re.search(line[::-1]).group()]
 
             calibration_sum += 10*first_digit + last_digit
 
