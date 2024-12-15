71d689525b725119187458f9f7c785ef90092751
Ian Roberts <i.roberts@dcs.shef.ac.uk>
2024-12-15 18:17:09 +0000

    2024 day 14 part 2 - just keep stepping until we hit something that looks too regular to be a co-incidence, and print it out for visual checking

--- 2024/14/robots.py
+++ 2024/14/robots.py
@@ -10,7 +10,7 @@ def load_data():
         for line in f:
             line = line.strip()
             px, py, vx, vy = (int(m.group()) for m in digits.finditer(line))
-            robots.append((Cell(py, px), Cell(vy, vx)))
+            robots.append([Cell(py, px), Cell(vy, vx)])
     return robots
 
 
@@ -28,5 +28,40 @@ def one_hundred_steps():
     print(f"safety: {reduce(mul, quadrants, 1)}")
 
 
+def matrix(occupied: set[Cell], rows: range, cols: range) -> str:
+    return "\n".join("".join("#" if Cell(r, c) in occupied else "." for c in cols) for r in rows)
+
+
+def easter_egg():
+    # It's not an Easter egg, it's a Christmas tree...
+    robots = load_data()
+    rows = range(103)
+    cols = range(101)
+    moves = 0
+
+    mat = matrix(set(r[0] for r in robots), rows, cols)
+    print(mat)
+
+    while True:
+        mat = matrix(set(r[0] for r in robots), rows, cols)
+        if "#############" in mat:
+            # Having that many robots in adjacent spaces is probably not a co-incidence
+            print(f"-------------- {moves} moves ---------------")
+            print(mat)
+            if input("Keep going? ") == "n":
+                break
+
+        # make one move
+        moves += 1
+        for r in robots:
+            p, v = r
+            r[0] = Cell((p.row + v.row) % 103, (p.col + v.col) % 101)
+
+        if (moves % 100) == 0:
+            print(moves)
+
+
+
 if __name__ == "__main__":
     one_hundred_steps()
+    easter_egg()
