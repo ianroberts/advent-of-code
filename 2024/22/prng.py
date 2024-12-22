568a56c6866ac016296fb519e5e86c37a51a266e
Ian Roberts <i.roberts@dcs.shef.ac.uk>
2024-12-22 17:33:36 +0000

    2024 day 22 part 2
    No, apparently we don't need to do anything clever with the bit twiddling, plain iteration over a sliding window of 4-tuples is sufficient.

--- 2024/22/prng.py
+++ 2024/22/prng.py
@@ -1,3 +1,8 @@
+import operator
+from collections import defaultdict
+from itertools import islice
+
+
 def load_data():
     with open("input", "r") as f:
         return [int(line) for line in f]
@@ -26,5 +31,42 @@ def part1():
     print(f"Total of 2000th numbers: {total}")
 
 
+def part2():
+    seeds = load_data()
+
+    all_prices = defaultdict(int)
+
+    for seed in seeds:
+        diffs = []
+        prices = []
+        for n in range(2000):
+            new_seed = prng(seed)
+            prices.append(new_seed % 10)
+            diffs.append(new_seed % 10 - seed % 10)
+            seed = new_seed
+
+        # this_buyer_sequences is a sliding window over this buyer's PRNG sequence,
+        # returning tuples of four items - (0, 1, 2, 3), (1, 2, 3, 4), (2, 3, 4, 5) ...
+        this_buyer_sequences = zip(
+            diffs,
+            islice(diffs, 1, None),
+            islice(diffs, 2, None),
+            islice(diffs, 3, None),
+        )
+        # Current price at the _first_ appearance of each 4-tuple
+        this_buyer_prices = {}
+        for price, seq in zip(islice(prices, 3, None), this_buyer_sequences):
+            if seq not in this_buyer_prices:
+                this_buyer_prices[seq] = price
+
+        # Update the global totals map from this buyer's numbers
+        for seq, price in this_buyer_prices.items():
+            all_prices[seq] += price
+
+    max_bananas = max(all_prices.items(), key=operator.itemgetter(1))
+    print(f"Max obtainable bananas: {max_bananas}")
+
+
 if __name__ == "__main__":
     part1()
+    part2()
