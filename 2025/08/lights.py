6c78cd66824c417350dc261e1737ac605354f258
Ian Roberts <i.roberts@sheffield.ac.uk>
2025-12-08 10:41:39 +0000

    2025 day 8 part 2 - same logic as part 1 but with a different termination condition (one circuit, rather than 1000 links)

--- 2025/08/lights.py
+++ 2025/08/lights.py
@@ -7,6 +7,14 @@ def load_input():
         return [tuple(int(i) for i in l.split(",")) for l in f]
 
 
+def part1_output(unique_circuits: dict[int, set[int]]):
+    # sort unique circuits by size
+    unique = list(unique_circuits.values())
+    unique.sort(key=len, reverse=True)
+
+    print(f"Product of 3 largest after 1000 connections: {reduce(operator.mul, (len(c) for c in unique[:3]), 1)}")
+
+
 def main():
     boxes = load_input()
     squared_distances: list[tuple[int, int, int]] = []
@@ -22,13 +30,11 @@ def main():
     # Initially each box is its own singleton circuit
     circuits: list[set[int]] = [{i} for i in range(len(boxes))]
     unique_circuits: dict[int, set[int]] = {id(c): c for c in circuits}
-    # iterate over the first thousand closest connections and merge the
+    # iterate over the connections from closes to furthest and merge the
     # circuits on each side of the connection - when two indices i and j
     # are part of the same circuit, then circuits[i] and circuits[j]
     # are *the same set object* (not just sets with the same members)
     for n, (i, j, d_ij) in enumerate(squared_distances):
-        if n == 1000:
-            break
         c_i = circuits[i]
         c_j = circuits[j]
         # First check if i and j are already in the same circuit - they might
@@ -44,11 +50,20 @@ def main():
             for member_i in c_i:
                 circuits[member_i] = c_j
 
-    # sort unique circuits by size
-    unique = list(unique_circuits.values())
-    unique.sort(key=len, reverse=True)
+        if n == 999:
+            # done the first thousand - this is where we stop for part 1
+            part1_output(unique_circuits)
 
-    print(f"Product of 3 largest: {reduce(operator.mul, (len(c) for c in unique[:3]), 1)}")
+        if len(unique_circuits) == 1:
+            # everything is one circuit - we're finished
+            break
+
+    # we've linked everything into one circuit, so the current
+    # values of i and j coming out of the loop are the last two
+    # boxes we connected in order to reach that state - multiply
+    # their x coords to get the final answer
+    print(f"last connection was box {i} at {boxes[i]} to box {j} at {boxes[j]}")
+    print(f"product of x coords = {boxes[i][0] * boxes[j][0]}")
 
 
 if __name__ == "__main__":
